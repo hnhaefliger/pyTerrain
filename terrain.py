73fa83dc663ab5b718b58fc5cd98ea821a502cd1
Henry Haefliger <54628404+hnhaefliger@users.noreply.github.com>
2019-10-29 13:23:00 +0100

    Add files via upload

--- terrain.py
+++ terrain.py
@@ -1,29 +1,45 @@
 import graphics.engine
 import perlin
+import math
 
 ############ Display variables
 
-scale = 8
+scale = 6
 distance = 100
 
 ############ Land size
 
-width = 100 # map width
-length = 50 # map length
+width = 150 # map width
+length = 100 # map length
 
 ############ Noise variables
 
-n1div = 20 # landmass distribution
-n2div = 3 # boulder distribution
-n3div = 1 # rock distribution
+n1div = 30# landmass distribution
+n2div = 5 # boulder distribution
+n3div = 0.5 # rock distribution
 
-n1scale = 20 # landmass height
-n2scale = 2 # boulder scale
-n3scale = 1 # rock scale
+n1scale = 30 # landmass height
+n2scale = 3 # boulder scale
+n3scale = 2 # rock scale
 
 noise1 = perlin.noise(width / n1div, length / n1div) # landmass / mountains
 noise2 = perlin.noise(width / n2div, length / n2div) # boulders
 noise3 = perlin.noise(width / n3div, length / n3div) # rocks
+
+############ z modifiers
+
+zroot = 2
+zpower = 2.5
+
+############ colors
+
+colors = {
+    0: 'blue',
+    1: 'yellow',
+    25: 'green',
+    30: 'gray',
+    1000: 'white'
+    }
 
 ############ 3D shapes
 
@@ -34,12 +50,10 @@ triangles = []
 
 def color(a, b, c): # check land type
     z = (points[a][2] + points[b][2] + points[c][2]) / 3 # calculate average height of triangle
-    if z <= 0:
-        return 'blue'
-    elif z < 1:
-        return 'yellow'
-    else:
-        return 'green'
+    for color in colors:
+        if z <= color:
+            return colors[color]
+            break
 
 for x in range(-int(width/2), int(width/2)):
     for y in range(-int(length/2), int(length/2)):
@@ -48,7 +62,11 @@ for x in range(-int(width/2), int(width/2)):
         z = noise1.perlin(x1 / n1div, y1 / n1div) * n1scale # add landmass
         z += noise2.perlin(x1 / n2div, y1 / n2div) * n2scale # add boulders
         z += noise3.perlin(x1 / n3div, y1 / n3div) * n3scale # add rocks
-        points.append([x, y, -z if z <= 0 else 0]) 
+        if z >= 0:
+            z = -math.sqrt(z)
+        else:
+            z = ((-z) ** (1 / zroot)) ** zpower
+        points.append([x, y, z]) 
 
 for x in range(width):
     for y in range(length):
@@ -62,6 +80,18 @@ for x in range(width):
 
 ############
 
-test = graphics.engine.Engine3D(points, triangles, scale=scale, distance=distance)
-test.rotate('x', -30)
-test.render()
+world = graphics.engine.Engine3D(points, triangles, scale=scale, distance=distance, width=1400, height=750)
+
+world.rotate('x', -30)
+world.render()
+
+'''
+def animate():
+    world.clear()
+    world.rotate('x', 30)
+    world.rotate('z', 0.1)
+    world.rotate('x', -30)
+    world.render()
+    world.screen.window.after(1, animate)
+animate()
+'''
